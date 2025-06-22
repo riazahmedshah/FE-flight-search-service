74d2516ac7921adce671c03ff19962d477ff7bae
riazahmedshah <riyazsh360@gmail.com>
2025-06-22 14:42:06 +0530

    added createFlight Route

--- src/routes/v1/FlightRoutes.ts
+++ src/routes/v1/FlightRoutes.ts
@@ -1,3 +1,6 @@
 import express from "express"
+import { createFlight } from "../../controllers/FlightController";
 
 export const v1FlightRoutes = express.Router();
+
+v1FlightRoutes.post("/flight", createFlight);
