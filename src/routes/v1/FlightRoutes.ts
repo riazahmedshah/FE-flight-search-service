a5bfcb9b822faf55454fb3661f1a55f8094471e2
riazahmedshah <riyazsh360@gmail.com>
2025-06-27 17:02:04 +0530

    added update flight route

--- src/routes/v1/FlightRoutes.ts
+++ src/routes/v1/FlightRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { createFlight, deleteFlight, getAllFlights, getFlight } from "../../controllers/FlightController";
+import { createFlight, deleteFlight, getAllFlights, getFlight, updateFlight } from "../../controllers/FlightController";
 
 export const v1FlightRoutes = express.Router();
 
-v1FlightRoutes.post("/flight", createFlight);
-v1FlightRoutes.get("/flight",  getAllFlights);
-v1FlightRoutes.get("/flight/:id", getFlight);
+v1FlightRoutes.post  ("/flight", createFlight);
+v1FlightRoutes.get   ("/flight",  getAllFlights);
+v1FlightRoutes.get   ("/flight/:id", getFlight);
+v1FlightRoutes.patch ("/flight/:id", updateFlight)
 v1FlightRoutes.delete("/flight/:id", deleteFlight);
