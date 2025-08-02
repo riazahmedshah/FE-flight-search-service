7c0feb8808b11ca5905bbf08052af4af1b0c19fc
riazahmedshah <riyazsh360@gmail.com>
2025-08-02 20:30:17 +0530

    minor imp in Airpport repo

--- src/repositories/AirportRepository.ts
+++ src/repositories/AirportRepository.ts
@@ -2,75 +2,50 @@ import { prisma } from "../config/db";
 
 export class AirportRepository {
     static async createAirport(name: string, address: string, city_id: number) {
-        try {
-            return await prisma.airport.create({
-                data: {
-                    name,
-                    address,
-                    city_id
-                }
-            });
-        } catch (error) {
-            console.error("Error creating airport:", error);
-        }
+        return await prisma.airport.create({
+            data: {
+                name,
+                address,
+                city_id
+            }
+        });
     }
 
     static async updateAirport(id: number, name: string, address:string) {
-        try {
-            return await prisma.airport.update({
-                where: { id },
-                data: { name, address }
-            });
-        } catch (error) {
-            console.error("Error updating airport:", error);
-        }
+        return await prisma.airport.update({
+            where: { id },
+            data: { name, address }
+        });
     }
 
     static async deleteAirport(id: number) {
-        try {
-            return await prisma.airport.delete({
-                where: { id }
-            });
-        } catch (error) {
-            console.error("Error deleting airport:", error);
-        }
+        return await prisma.airport.delete({
+            where: { id }
+        })
     }
 
     static async getAirport(id: number) {
-        try {
-            return await prisma.airport.findUnique({
-                where: { id },
-                include: { city: true }
-            });
-            
-        } catch (error) {
-            console.error("Error fetching airport:", error);
-        }
+        return await prisma.airport.findUnique({
+            where: { id },
+            include: { city: true }
+        });
     }
 
     static async findAirportByName(name: string) {
-        try {
-            return await prisma.airport.findFirst({
-                where: {
-                    name: {
-                        equals: name,
-                        mode: 'insensitive'
-                    }
+        return await prisma.airport.findFirst({
+            where: {
+                name: {
+                    equals: name,
+                    mode: 'insensitive'
                 }
-            });
-        } catch (error) {
-            console.error("Error finding airport by name:", error);
-        }
+            }
+        });
     }
 
     static async getAllAirports() {
-        try {
-            return await prisma.airport.findMany({
-                include: { city: true },
-                orderBy: { name: 'asc' }
-            });
-        } catch (error) {
-            console.error("Error fetching all airports:", error);
-        }
+        return await prisma.airport.findMany({
+            include: { city: true },
+            orderBy: { name: 'asc' }
+        });
     }
 }
